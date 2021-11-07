7c68094aead046c7a56e99fc9343fcf16a297222
ILIU <iliu@yandex.ru>
2021-11-07 20:43:41 +0300

    first step

---
+++ solution/index.js
@@ -0,0 +1,58 @@
+module.exports = class {
+    body;
+    size;
+    [Symbol.iterator] () {
+        const self = this;                
+        let current = 0;        
+        return {
+            next () {                
+                if (current < self.size)                    
+                    return {
+                        done: false,
+                        value: self.body[current++]
+                    }
+                else {
+                    return {
+                        done: true
+                    }
+                }
+            }
+        }
+    }
+    constructor (...theArgs) {
+        this.size = 0;
+        this.body = [];        
+        if (theArgs.length) {            
+            for (let i = 0; i < theArgs.length; i++) {
+                this.add(theArgs[i]);
+            }
+        } 
+    }    
+    keys () {
+        return this.body;
+    }
+    values(){
+        return this.body;
+    }
+    entries() {               
+        return this.body.map(el => el = [el, el])    
+    }
+
+    clear(){
+        this.size = 0;
+        this.body = [];
+    }
+    add(item){                        
+        if (!(this.body.includes(item))) {            
+            this.body[this.size] = item;            
+            this.size++;            
+        }
+        return this;
+    }
+    delete(item){
+        this.body = this.body.filter(el => el !== item);
+    }
+    has(item){
+        return this.body.includes(item);
+    }
+}
